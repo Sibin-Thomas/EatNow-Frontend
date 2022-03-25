4e08ec8bcf0eb398082367d951968b75668fa276
Sibin-Thomas <sibintho007@gmail.com>
2022-03-25 10:26:09 +0530

    login page creation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {username: '', password: ''};
+
+    this.handleUsernameChange = this.handleUsernameChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+
+  handleUsernameChange(event) {
+    this.setState({username: event.target.value});
+  }
+  handlePasswordChange(event) {
+    this.setState({password: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.username + ' ' + this.state.password);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="row align-items-center">
+        <div className="col">
+        </div>
+        <div className="col border border-5 border-primary bg-light">
+          <form onSubmit={this.handleSubmit}>
+              <label>Username:</label>
+              <br/>
+              <input type="text" value={this.state.username} onChange={this.handleUsernameChange} />
+              <br/>
+              <label>Password:</label>
+              <br/>
+              <input type="text" value={this.state.password} onChange={this.handlePasswordChange} />
+              <br/>
+            <input className="bg-warning"type="submit" value="Submit" />
+          </form>
+        </div>
+        <div className="col">
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
