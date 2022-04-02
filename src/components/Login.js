7f3c7f11e9490e743dba51830a76d7d768e66e7e
Sibin-Thomas <sibintho007@gmail.com>
2022-04-02 10:44:45 +0530

    created register page and added routes

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,36 +1,51 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Branding from "./Branding";
 
 class Login extends React.Component {
 
     render()
     {
         return (
-            <div className="container p-2 border-bottom border-top mt-3">
-                <div className="container d-flex justify-content-center p-3 h3">Please Sign In</div>
-                <div className="row w-100">
-                    <div className="col"/>
-                    <div className="col-3">
-                        <input type="text" className="form-control-lg shadow-none" id="username" placeholder="Username"/>
-                    </div>
-                    <div className="col"/>
-                </div>
-                <div className="row  w-100 mt-1">
-                    <div className="col"/>
-                    <div className="col-3">
-                        <input type="password" className="form-control-lg" id="password" placeholder="Password"/>
-                    </div>
-                    <div className="col"/>
-                </div>
-                <div className="row w-100 mt-1">
-                    <div className="col"/>
-                    <div className="col-3 d-flex">
-                        <input type="submit" className="form-control-lg btn-primary" id="submit" placeholder="Submit"/>
-                        <div className="container ml-3">
-                        New User?
-                        <a href="www.google.com">Register here</a>
+            <div>
+                <Branding/>
+                <div className="container p-2 border-bottom border-top mt-3">
+                    <div className="container d-flex justify-content-center p-3 h3">Please Sign In</div>
+                    <div className="row w-100">
+                        <div className="col"/>
+                        <div className="col-3">
+                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                        <label class="form-check-label" for="flexCheckDefault">
+                            Restaurant
+                        </label>
                         </div>
+                        <div className="col"/>
                     </div>
-                    <div className="col" />
+                    <div className="row w-100 mt-3">
+                        <div className="col"/>
+                        <div className="col-3">
+                            <input type="text" className="form-control-lg shadow-none" id="username" placeholder="Username"/>
+                        </div>
+                        <div className="col"/>
+                    </div>
+                    <div className="row  w-100 mt-1">
+                        <div className="col"/>
+                        <div className="col-3">
+                            <input type="password" className="form-control-lg" id="password" placeholder="Password"/>
+                        </div>
+                        <div className="col"/>
+                    </div>
+                    <div className="row w-100 mt-1">
+                        <div className="col"/>
+                        <div className="col-3 d-flex">
+                            <input type="submit" className="form-control-lg btn-primary" id="submit" placeholder="Submit"/>
+                            <div className="container ml-3">
+                            New User?
+                            <Link to='/register'>Register here</Link>
+                            </div>
+                        </div>
+                        <div className="col" />
+                    </div>
                 </div>
             </div>
         );
--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './components/App';
+import Register from './components/Register';
 
 ReactDOM.render(
-  <App/>,
+
+  <BrowserRouter>
+    <Routes>
+        <Route path='/' element={<App/>} />
+        <Route path='/register' element={<Register/> } />
+    </Routes>
+  </BrowserRouter>
+  ,
   document.getElementById('root')
 );
 
